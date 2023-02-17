0eb4aac2b76123368e4f28124e2fb9818d795d08
Astro2030 <omariwycliffe5@gmail.com>
2023-02-17 08:00:29 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints numbers zero to nine
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
