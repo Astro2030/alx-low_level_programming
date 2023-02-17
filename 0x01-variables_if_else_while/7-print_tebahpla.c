bd8c98c93b8dff56daf53017ec84e09212d44e8f
Astro2030 <omariwycliffe5@gmail.com>
2023-02-17 08:08:29 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a prograzm that prints the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char lowercase_letters[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int x;
+
+	for (x = 0; x < 26; x++)
+	{
+		putchar(lowercase_letters[x]);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
