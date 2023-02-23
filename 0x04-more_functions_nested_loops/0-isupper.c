8db37b526e2e10c95a3e4c4dff6350cccc7f0e8e
Astro2030 <omariwycliffe5@gmail.com>
2023-02-23 05:45:25 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - function that checks for uppercase character
  * Return: 1 if is uppercase and 0 if otherwise
- * @x: number to be checked
+ * @c: number to be checked
  */
 
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 99)
+	if (c >= 65 && c <= 99)
 	{
 		return (1);
 	}
