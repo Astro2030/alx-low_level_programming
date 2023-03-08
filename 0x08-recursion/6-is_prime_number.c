c4c1955dd3d23e58e4bd4ddbcc87831c8d24c64c
Astro2030 <omariwycliffe5@gmail.com>
2023-03-08 06:43:38 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * is_divisible - checks if a number is divisible
+ * @num: the number to be checked
+ * @div: The divisor.
+ * Return: 0(if divisible) and 1(not divisible)
+ */
+
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
