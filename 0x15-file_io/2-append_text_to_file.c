e5a36319d688a348b350e413ad5b86ec2dea0618
Astro2030 <omariwycliffe5@gmail.com>
2023-04-10 05:13:52 -0700

    appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	o = open(filename, O_WRONLY | O_APPEND);
 	g = write(o, text_content, length);
 
-	if (o == -1 || w == -1)
+	if (o == -1 || g == -1)
 	{
 		return (-1);
 	}
